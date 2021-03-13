f66149e32b8a560f639d07dd6aade7399c270a0e
Daniel Kowalski <dek6613@rit.edu>
2021-03-13 14:02:52 -0500

    Worked on player controls

--- Low Effort Jam 8/Assets/Scripts/PlayerController.cs
+++ Low Effort Jam 8/Assets/Scripts/PlayerController.cs
@@ -8,11 +8,24 @@ public class PlayerController : MonoBehaviour
 {
     public MatterState state;
 
+    // Movement fields
     [SerializeField]
     private Rigidbody2D rigidbody;
 
+    private float xVel;
+    private float yVel;
+
     [SerializeField]
     private float gravity = 9.8f;
+
+    [SerializeField]
+    private float downMaxSpeed;
+
+    [SerializeField]
+    private float upAccel;
+
+    [SerializeField]
+    private float upMaxSpeed;
 
     [SerializeField]
     private float lrAccel;
@@ -20,25 +33,48 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float lrMaxSpeed;
 
+    [SerializeField]
+    private float lrDeceleration;
+
+    [SerializeField]
+    private float lrIceDeceleration;
+
+    // Temperature fields
     public float temperature;
+
+    [SerializeField]
+    private float heatLossRate;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        xVel = rigidbody.velocity.x;
+        yVel = rigidbody.velocity.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Movement
         bool left = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
         bool right = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
-        bool up = Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow);
+
+        xVel = rigidbody.velocity.x;
+        yVel = rigidbody.velocity.y;
+
+        if (left)
+        {
+            xVel -= lrAccel;
+        }
+        if (right)
+        {
+            yVel += lrAccel;
+        }
 
         switch (state)
         {
             case MatterState.Solid:
-                SolidUpdate(left, right, up);
+                SolidUpdate(left, right);
                 break;
             case MatterState.Liquid:
                 LiquidUpdate(left, right);
@@ -48,21 +84,42 @@ public class PlayerController : MonoBehaviour
                 break;
         }
 
-        Mathf.Clamp(rigidbody.velocity.x, -lrMaxSpeed, lrMaxSpeed);
+        xVel = Mathf.Clamp(xVel, -lrMaxSpeed, lrMaxSpeed);
+        yVel = Mathf.Clamp(yVel, -downMaxSpeed, upMaxSpeed);
+
+        rigidbody.velocity = new Vector2(xVel, yVel);
+
+        // Temperature
+        temperature -= heatLossRate * Time.deltaTime;
     }
 
-    void SolidUpdate(bool left, bool right, bool up)
+    void SolidUpdate(bool left, bool right)
     {
+        if (!(left || right))
+        {
+            xVel /= lrIceDeceleration;
+        }
 
+        yVel -= gravity;
     }
 
     void LiquidUpdate(bool left, bool right)
     {
+        if (!(left || right))
+        {
+            xVel /= lrDeceleration;
+        }
 
+        yVel -= gravity;
     }
 
     void GasUpdate(bool left, bool right)
     {
+        if (!(left || right))
+        {
+            xVel /= lrDeceleration;
+        }
 
+        yVel += upAccel;
     }
 }
