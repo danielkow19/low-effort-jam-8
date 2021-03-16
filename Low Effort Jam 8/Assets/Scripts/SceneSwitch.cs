0d46b996d5633eed15a0c83d351571b52ab4c024
dek6613 <dek6613@rit.edu>
2021-03-16 01:42:29 -0400

    Created loading zone prefab

---
+++ Low Effort Jam 8/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private string nextScene;
+
+    [SerializeField]
+    private Collider2D player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.Equals(player))
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
